71de671b43652c927bb08d0581d5b1108d3ec482
Kyle Deguzman <kyledeguzman147@yahoo.com>
2021-06-22 05:34:19 +0000

    created an action for contact form to save to database

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,20 @@
 class ContactsController < ApplicationController 
-    def new
+  def new
+    @contact = Contact.new
+  end
+  
+  def create 
+    @contact = Contact.new(contact_params)
+    
+    if @contact.save 
+      redirect_to new_contact_url, notice: "Thank you, your message has been sent!"
+    else 
+      redirect_to new_contact_path, notice: "Error occurence, message not sent."
+    end
+  end 
+  
+  private 
+    def contact_params 
+      params.require(:contact).permit(:name, :email, :comments)
     end
 end
